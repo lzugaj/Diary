082e498ffdbce1fd36e120f2905d8d1bf53321a4
lzugaj <luka.zugaj7@gmail.com>
2020-11-07 15:07:21 +0100

    Initial commit

---
+++ src/test/java/com/luv2code/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
