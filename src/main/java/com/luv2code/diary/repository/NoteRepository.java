ad76ae15ec63d0e5c05a3979cb0b34e08a89e8ea
lzugaj <luka.zugaj7@gmail.com>
2020-11-16 15:25:47 +0100

    Implement note logic
    Create repository

---
+++ src/main/java/com/luv2code/diary/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package com.luv2code.diary.repository;
+
+import com.luv2code.diary.domain.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
